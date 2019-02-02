95a21ed58e06f63caa67a288d4efdaa9de62b56b
Dan <dantran2016@gmail.com>
2019-02-02 01:48:04 -0800

    Changed directory name typo

--- server/gradientGen.js
+++ server/gradientGen.js
@@ -6,14 +6,13 @@ const path = "images/pixel"
 var data = [0, 150, 255, 24, 100, 239, 1, 240, 0, 200];
 
 gm(1, 1, "rgb(" + data[0] + "," + data[0] + "," + data[0] + ")")
-.write("images/test.png", function (err) {
+.write("images/output.png", function (err) {
 });
 
 for (let i = 1; i < data.length; i++) {
     gm(1, 1, "rgb(" + data[i] + "," + data[i] + "," + data[i] + ")")
         .write(path + i + ".png", function (err) {
         });
-    gm("images/test.png").append(path + i + ".png", true).write("images/output.png", function (err) { });
+    gm("images/output.png").append(path + i + ".png", true).write("images/output.png", function (err) { });
 }
-
 
