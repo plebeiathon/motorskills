4b199c5a83a7cac0019bc0bf90d19e434e3729eb
Dan <dantran2016@gmail.com>
2019-02-02 01:39:57 -0800

    Image creator finished

--- server/gradientGen.js
+++ server/gradientGen.js
@@ -1,10 +1,19 @@
 var fs = require('fs')
 var gm = require('gm');
 
-const path = "brandNewImg.jpg"
+const path = "images/pixel"
 
-gm(200, 400, "#ddff99f3")
-    .drawText(10, 50, "from scratch")
-    .write(path, function (err) {
+var data = [0, 150, 255, 24, 100, 239, 1, 240, 0, 200];
 
-    });
+gm(1, 1, "rgb(" + data[0] + "," + data[0] + "," + data[0] + ")")
+.write("images/test.png", function (err) {
+});
+
+for (let i = 1; i < data.length; i++) {
+    gm(1, 1, "rgb(" + data[i] + "," + data[i] + "," + data[i] + ")")
+        .write(path + i + ".png", function (err) {
+        });
+    gm("images/test.png").append(path + i + ".png", true).write("images/output.png", function (err) { });
+}
+
+
