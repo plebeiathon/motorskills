5afc9b350f403f0cea00ef844983cdc4394adc3d
Adrian Darian <adarian@ucmerced.edu>
2019-02-02 10:59:55 -0800

    demo

--- server/gradientGen.js
+++ server/gradientGen.js
@@ -1,6 +1,6 @@
 var gm = require('gm');
+var fs = require('fs')
 
-const path = "images/pixel"
 var data = [];
 let time = (new Date).getTime();
 
@@ -8,15 +8,22 @@ for (var i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
         data[j] = Math.random() * 255 + 1;
     }
-    gm(1, 1, "rgb(" + data[0] + "," + data[0] + "," + data[0] + ")")
-        .write("images/Outputs/output-" + time + ".png", function (err) {
+    gm(1, 1, `rgb(${data[0]}, ${data[0]}, ${data[0]})`)
+        .write(`images/Outputs/output-${time}.png`, function (err) {
         });
 
     for (let j = 1; j < data.length; j++) {
-        gm(1, 1, "rgb(" + data[j] + "," + data[j] + "," + data[j] + ")")
-            .write(path + j + ".png", function (err) {
+        gm(1, 1, `rgb(${data[j]}, ${data[j]}, ${data[j]})`)
+            .write(`images/pixel${j}.png`, function (err) {
             });
-        gm("images/Outputs/output-" + time + ".png").append(path + j + ".png", true).write("images/Outputs/output-" + time + ".png", function (err) { });
+        gm(`images/Outputs/output-${time}.png`).append(`images/pixel${j}.png`, true).write(`images/Outputs/output-${time}.png`, function (err) { });
     }
-}
 
+    //gm('./motor.csv').append('gs://greyscale/img/output-' + time + '.png', true);
+    
+    // fs.writeFileSync('motor.csv', `gs://greyscale/img/output-${time}.png`);
+    fs.appendFile('motor.csv', `gs://greyscale/img/output-${time}.png\n`, 'utf8', (err) => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+      })
+}
