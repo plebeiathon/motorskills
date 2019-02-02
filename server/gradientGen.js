080f5935f4aa5f47797f309f91197a282989e733
Adrian Darian <adarian@ucmerced.edu>
2019-02-02 05:20:18 -0800

    working dan code with time

--- server/gradientGen.js
+++ server/gradientGen.js
@@ -2,21 +2,21 @@ var gm = require('gm');
 
 const path = "images/pixel"
 var data = [];
+let time = (new Date).getTime();
 
 for (var i = 0; i < 10; i++) {
     for (let j = 0; j < 10; j++) {
         data[j] = Math.random() * 255 + 1;
     }
-
     gm(1, 1, "rgb(" + data[0] + "," + data[0] + "," + data[0] + ")")
-        .write("images/Outputs/output" + i + ".png", function (err) {
+        .write("images/Outputs/output-" + time + ".png", function (err) {
         });
 
     for (let j = 1; j < data.length; j++) {
         gm(1, 1, "rgb(" + data[j] + "," + data[j] + "," + data[j] + ")")
             .write(path + j + ".png", function (err) {
             });
-        gm("images/Outputs/output" + i + ".png").append(path + j + ".png", true).write("images/Outputs/output" + i + ".png", function (err) { });
+        gm("images/Outputs/output-" + time + ".png").append(path + j + ".png", true).write("images/Outputs/output-" + time + ".png", function (err) { });
     }
 }
 
