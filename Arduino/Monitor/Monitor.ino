fc36d5d401c922fe4e75e97e6f84b1219c6494d1
Brian <brianhungerman@gmail.com>
2019-02-02 17:56:22 -0800

    Update Monitor.ino

--- Arduino/Monitor/Monitor.ino
+++ Arduino/Monitor/Monitor.ino
@@ -1,53 +1,39 @@
-/*
-  Software serial multple serial test
+#define encoder0PinA 2
+#define encoder0PinB 3
 
- Receives from the hardware serial, sends to software serial.
- Receives from software serial, sends to hardware serial.
-
- The circuit:
- * RX is digital pin 10 (connect to TX of other device)
- * TX is digital pin 11 (connect to RX of other device)
-
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts,
- so only the following can be used for RX:
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
-
- Not all pins on the Leonardo and Micro support change interrupts,
- so only the following can be used for RX:
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
-
- created back in the mists of time
- modified 25 May 2012
- by Tom Igoe
- based on Mikal Hart's example
-
- This example code is in the public domain.
-
- */
 #include <SoftwareSerial.h>
 
+int encoder0Pos = 0;
+
+int valRotary,lastValRotary;
+
 SoftwareSerial mySerial(10, 11); // RX, TX
-int count = 0;
+
 void setup() {
-  // Open serial communications and wait for port to open:
- Serial.begin(9600);
-  // set the data rate for the SoftwareSerial port
+  pinMode(encoder0PinA, INPUT_PULLUP);
+  pinMode(encoder0PinB, INPUT_PULLUP);
+  attachInterrupt(0, doEncoder, CHANGE);
+  
   mySerial.begin(9600);
-//  mySerial.println("Hello, world?");
 }
 
-void loop() { // run over and over
-  //read sensor, communicate?
-  //int val = analogRead(0);
-//  for (int i = 0; i < 10; i++) { mySerial.write(i * i ); delay(10`0);}
- count++;
- mySerial.write(map(analogRead(0), 0, 1024, 10, 250));
-//  mySerial.write(100);
-// Serial.println(map(analogRead(0), 0, 1024, 10, 250));
-Serial.println(analogRead(A0));
-//Serial.print("-->");
-//Serial.println(analogRead(A1));
-  delay(300);
-}
+void loop() {
+   lastValRotary = valRotary;
+   delay(250);
 
+   if (encoder0Pos < 0) encoder0Pos = -encoder0Pos;
+   mySerial.write(encoder0Pos * 4);
+   encoder0Pos = 0;
+}
+void doEncoder()
+{
+  if (digitalRead(encoder0PinA) == digitalRead(encoder0PinB))
+  {
+    encoder0Pos++;
+  }
+  else
+  {
+    encoder0Pos--;
+  }
+  valRotary = encoder0Pos/2.5;
+}
